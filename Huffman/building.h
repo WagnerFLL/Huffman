95ab9525ae68eaae73b9c223089220fa2caf0d48
Wagner Fontes <wsf@ic.ufal.br>
2017-05-03 17:02:05 -0300

    Update building.h

--- Huffman/building.h
+++ Huffman/building.h
@@ -5,6 +5,7 @@ Tree *func_exception(FILE *arquivo);
 
 /*
  * Constrói a árvore de huffman a partir do cabeçalho do arquivo.
+ * Recebe o ponteiro do arquivo e retorna um ponteiro para a árvore.
  */
 Tree* rebuild_huffman_tree (FILE *arquivo);
 
@@ -14,14 +15,13 @@ Tree* rebuild_huffman_tree (FILE *arquivo);
 Tree* build_huffman_tree(int *array);
 
 /*
- * Recebe uma fila e monta a árvore de huffman.
+ * Recebe uma fila e monta a árvore de huffman. Retorna a árvore.
  */
 Tree *build_Tree(priority_queue *pq);
 
 /*
  * Recebe um ponteiro para a árvore de huffman e inicia o processo que gera a partir dela uma string em pré-ordem. 
  * Recebe um ponteiro para o arquivo de saída e escreve dois bytes vazios no início.
+ * Retorna o tamaho da árvore.
  */
 int creating_huffman_string(Tree *huffman, FILE *header);
-
-// gcc main.c compress.h compress.c decompress.h decompress.c building.h building.c tree.h tree.c queue.h queue.c -o test2.exe
